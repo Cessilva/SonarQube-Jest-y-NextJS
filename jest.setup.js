718252ac8aee1edd2102fbfdabd17e548ee74a6c
cecilia_developer <shugar.developer@gmail.com>
2022-10-31 17:50:44 -0600

    cambios

---
+++ jest.setup.js
@@ -0,0 +1,8 @@
+// Optional: configure or set up a testing framework before each test.
+// If you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Used for __tests__/testing-library.js
+// The @testing-library/jest-dom library provides a set of custom jest matchers that you can use to extend jest. 
+// These will make your tests more declarative, clear to read and to maintain.
+// Learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom/extend-expect'
